759edfd6e70ea2d41662911884c2957c4238963f
WindBeat <jordanwalker465@gmail.com>
2014-01-16 20:26:03 -0800

    Cleaned up JS code
    Changed the if/else statements to case.

--- application.js
+++ application.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	
     var answer = Math.floor((Math.random()*30)+1);
+    
 	
 	$('#guess').keypress(function(event) {
         if(event.keyCode==13){
@@ -10,28 +11,25 @@ $(document).ready(function() {
 
 	$('#submit').click(function() {
 		var guess = $('#guess').val();
-		
-		if(guess >= 31 || guess <= 0) {
-			$('#result').text("Number must be between 1 and 30.");
-			return false;
-		};
-		
-		if(isNaN(guess)) {
-			$('#result').text("Not sure what you entered, but it's not a number.");
-		};
-
 		var diff = Math.abs(guess - answer);
-
-		if(diff == 0 )	{
-			$('#result').text("That is correct!!");
-		}
-
-		else if(diff <= 5) {
-			$('#result').text("Hot!");
-		}
-
-		else if(diff > 5) {
-			$('#result').text("Cold..");
+		switch(true) {
+			case (guess >= 31 || guess <= 0) :
+				$('#result').text("Number must be between 1 and 30.");
+				return false;
+				break;
+			case (isNaN(guess)) :
+				$('#result').text("Not sure what you entered, but it's not a number.");
+				break;
+			case (diff == 0 ) :
+				$('#result').text("That is correct!!");
+				break;
+			case (diff <= 5) :
+				$('#result').text("Hot!");
+				break;
+			case (diff > 5) :
+				$('#result').text("Cold..");
+				break;				
+			default : 
 		};
 		
 	});
